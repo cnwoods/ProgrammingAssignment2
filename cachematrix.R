9aafeef77f83c46cf8419ec3a0f71fc6d3450325
cnwoods <cnwoods@users.noreply.github.com>
2015-02-23 10:02:18 +1100

    Debugged version
    Finally figured the trick to get it to work without 'atomic vector'
    error.

--- cachematrix.R
+++ cachematrix.R
@@ -6,24 +6,22 @@
 ## of a matrix in a cache, allowing it to be used by other functions.
 
 makeCacheMatrix <- function(x = matrix()) {
-      ## these assignments 'clean the slate' for the function.
-      ## note that 'set' is a nested function so <<- is needed to
-      ## ensure the values will be available to the makeCacheMatrix
-      ## environment (since it encloses the 'set' environment)
-      s <- NULL
-    set <- function(y) {
-      x <<- y
-      s <<- NULL
-    }
-     ## these assignments are back out in the main environment, but
-     ## again setinv nests a function within a function, hence <<-
-    get <- function() x
-    setinv <- function(solve) s <<- solve
-    getinv <- function() s
-     ## this returns the object that holds the inverse of matrix x
-    list(set = set, get = get,
-         setinv = setinv,
-         getinv = getinv)
+  ## these assignments 'clean the slate' for the function.
+  ## note that 'set' is a nested function so <<- is needed to
+  ## ensure the values will be available to the makeCacheMatrix
+  ## environment (since it encloses the 'set' environment)
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  ## these assignments are back out in the main environment, but
+  ## again setinv nests a function within a function, hence <<-
+  get <- function() x
+  setinv <- function(solve) s <<- solve
+  getinv <- function() s
+  ## this returns the object that holds the inverse of matrix x
+  xx<-list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -31,18 +29,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## it has, so long as the matrix is unchanged, returns the solution 
 ## from cache.  If not, then the inverse is calculated and returned.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        ## checks to see if there is a value in cache and displays
-        ## a message if cached values are found, then returns them.
-    s <- x$getinv()
-    if(!is.null(s)) {
-      message("getting cached data")
-      return(s)
-    }
-        ## calculates the inverse of the matrix, and auto-prints it
-    data <- x$get()
-    s <- solve(data, ...)
-    x$setinv(s)
-    s
+cacheSolve <- function(xx, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  ## checks to see if there is a value in cache and displays
+  ## a message if cached values are found, then returns them.
+  s <- xx$getinv()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  ## calculates the inverse of the matrix, and auto-prints it
+  data <- xx$get()
+  s <- solve(data, ...)
+  xx$setinv(s)
+  s
 }
